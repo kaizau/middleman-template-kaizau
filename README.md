ac65b22d970d4df240bd3e6db27f54d0a5430303
Yifei Zhang <yz@yifei.co>
2012-03-31 22:53:27 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Middleman Template: YZ
+
+* A static project starter, groomed for Heroku
+* Uses middleman-3.0.0.beta.\*
+
+First:
+
+```bash
+git clone git://github.com/ezYZ/middleman-template-yz.git
+mv middleman-template-yz/yz/ ~/.middleman/
+```
+
+Then:
+
+```bash
+middleman init my-new-project --template=yz
+```
---
+++ yz/config.rb
@@ -0,0 +1,43 @@
+require 'pp'
+
+# Project Dir 
+set :css_dir, 'css'
+set :js_dir, 'js'
+set :images_dir, 'img'
+
+# Compass
+compass_config do |c|
+  c.output_style = :expanded
+end
+
+# Haml
+activate :automatic_image_sizes
+helpers do
+  def debug obj
+    "\n<pre>\n\n#{h obj.pretty_inspect}\n</pre>\n\n"
+  end
+
+  def dev_header
+    if environment == :development
+      "<!--\n"\
+      "  DEVELOPMENT SITE\n"\
+      "  Last Updated: #{DateTime.now.strftime('%b %d, %Y - %H:%M:%S')}\n"\
+      "-->\n"\
+      "<meta name='robots' content='noindex, nofollow' />\n"
+    end
+  end
+end 
+
+# Build
+configure :build do
+  compass_config {|c| c.output_style = :compressed}
+
+  activate :relative_assets
+  #activate :minify_javascript
+  #activate :cache_buster
+
+  ignore '/css/lib/bourbon/lib/bourbon/sass_extensions/functions.rb'
+  ignore '/css/lib/bourbon/lib/bourbon/sass_extensions/functions/compact.rb'
+  ignore '/css/lib/bourbon/lib/bourbon/sass_extensions.rb'
+  ignore '/css/lib/bourbon/lib/bourbon.rb'
+end
