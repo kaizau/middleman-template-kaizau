24b95e798561fff7cd65e21029b1bbb9a8b3f252
Kai Zau <kzau@groupon.com>
2013-10-01 16:33:46 -0700

    Updates jquery version, cleans up modernizer helper

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -16,7 +16,7 @@ module CustomHelpers
     page = 'index' if page == '/'
     page[0] = '' if page[0] == '/'
     request.path == page ||
-      request.path == "#{page}.html" || 
+      request.path == "#{page}.html" ||
       request.path == "#{page.chomp('.html')}/index.html"
   end
 
@@ -26,10 +26,10 @@ module CustomHelpers
 
   ##
   # Helper for generating a <span> if you're on the page, but an <a> otherwise
-  # 
-  # Padrino doesn't wrap the false condition of `link_to :if => ...` in a <span>. 
+  #
+  # Padrino doesn't wrap the false condition of `link_to :if => ...` in a <span>.
   # This is a workaround to produce more styleable output.
-  # 
+  #
   def active_link(name, page, active = 'active')
     if page?(page)
       "<span class='#{active}'>#{name}</span>"
@@ -56,7 +56,7 @@ module CustomHelpers
   end
 
   ##
-  # Never forget your development meta tags again 
+  # Never forget your development meta tags again
   #
   def dev_header
     if development?
@@ -71,20 +71,19 @@ module CustomHelpers
   ##
   # Outputs modernizr script tags, with appropriate jquery path
   #
-  # Scripts should be added as a page variable (array). 
-  # External paths and paths prefixed with '!' are not altered.
-  # jQuery CDN path is automatically added on production for 'jquery'.
+  # Scripts should be added as a page variable (array).
+  # External paths and paths prefixed with "!" are not altered.
+  # jQuery CDN path is automatically added on production for "jquery".
   #
   def modernizr_scripts
     if data.page.javascripts.is_a?(Array)
-      jquery = (! development?) ? '//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js' : asset_path(:js, 'vendor/jquery.min')
-      scripts = data.page.javascripts.map do |script| 
-        if script[0,2] == '//' || script[0,7] == 'http://' || script[0,8] == 'https://'
-          # nothing
-        elsif script[0] == '!'
+      scripts = data.page.javascripts.map do |script|
+        if script[0,2] == "//" || script[0,7] == "http://" || script[0,8] == "https://"
+          # use as is
+        elsif script[0] == "!"
           script = script[1..-1]
-        elsif script == 'jquery'
-          script = jquery
+        elsif script == "jquery"
+          script = development? ? asset_path(:js, "vendor/jquery.min") : "//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"
         else
           script = asset_path :js, script
         end
@@ -92,13 +91,13 @@ module CustomHelpers
         "'#{script}'"
       end
 
-      output = javascript_include_tag 'vendor/modernizr.min'
-      output << %Q|\n<script>\n  Modernizr.load([\n    #{scripts.join(",\n    ")}\n  ]);\n</script>|
+      output = javascript_include_tag "vendor/modernizr.min"
+      output << %Q|\n<script>\n  Modernizr.load([\n    #{scripts.join(",\n    ")}\n  ]);\n</script>|.html_safe
     else
       # No Modernizr means HTML5 elements still need to init'ing in old IE
 %q|<!--[if lt IE 9]>
   <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.6.1/html5shiv.min.js"></script>
-<![endif]-->|
+<![endif]-->|.html_safe
     end
   end
 
